fa741fafb57f981796093917ecc01626c230c51e
MansiVaghasiya <mansi@biznovare.com>
2024-09-27 09:44:43 +0530

    refactor

--- src/main/java/org/example/controller/VerticleController.java
+++ src/main/java/org/example/controller/VerticleController.java
@@ -38,7 +38,7 @@ public class VerticleController extends AbstractVerticle {
         router.get("/completed_tasks").handler(routingContext -> controllerMoethods.getAllCompletedTasks(routingContext, engine));
         router.get("/uncompleted_tasks").handler(routingContext -> controllerMoethods.unCompletedTaskPage(routingContext, engine));
 
-        router.get("/save_task").handler(routingContext -> controllerMoethods.saveTask(routingContext));
+        router.get("/save-task").handler(routingContext -> controllerMoethods.saveTask(routingContext));
         router.get("/edit-task").handler(routingContext -> controllerMoethods.editTask(routingContext));
         router.get("/delete").handler(routingContext -> controllerMoethods.deleteTask(routingContext));
         router.get("/openCreatEditModal").handler(routingContext -> controllerMoethods.openCreateEditTaskModal(routingContext, engine));
